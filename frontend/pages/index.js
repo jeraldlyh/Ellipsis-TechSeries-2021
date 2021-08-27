84dd6388cc24643701a90fed3c3ff862072a1584
Jerald <37609749+jeraldlyh@users.noreply.github.com>
2021-08-27 16:45:32 +0800

    Initialise project files

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <span className="text-red-500">test</span>
+        </div>
+    )
+}
