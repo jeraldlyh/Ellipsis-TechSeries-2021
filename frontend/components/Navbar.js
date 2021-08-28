49484f44433dc3ceb7ce02ba8f75d65d57ef083e
Felice <felicepng2010@gmail.com>
2021-08-28 11:13:14 +0800

    Add navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,7 +1,12 @@
 export default function Navbar() {
   return (
-    <div className="font-semibold flex h-10 bg-white items-center justify-between p-6">
-      <div>CHEQUEMATE</div>
+    <div className="font-semibold flex bg-white items-center justify-between px-10 pt-1 pb-3">
+      <div className="tracking-widest">CHEQUEMATE</div>
+      <div className="flex items-center justify-between w-56">
+        <div className="tracking-wider text-xs">Help</div>
+        <div className="tracking-wider text-xs">Login</div>
+        <div className="py-2 px-5 tracking-wider bg-black rounded-md text-white text-xs">Register</div>
+      </div>
     </div>
   )
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,9 +2,9 @@ import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col min-h-screen py-2">
       <Navbar />
-      <div className="text-red-500">test</div>
+      <div className="bg-gray-50 w-screen h-screen">test</div>
     </div>
   )
 }
