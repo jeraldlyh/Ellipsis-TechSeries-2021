36e3e3854d0a1a5ab8c4c2f12fbbacb486e10e16
Felice <felicepng2010@gmail.com>
2021-08-28 13:06:41 +0800

    Add description

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -26,16 +26,26 @@ export default function Home() {
         <div className="mt-8 mb-20 text-red-700 font-light text-center text-xxs">
           - CHEQUEMATE
         </div>
-        <img src="/landing/background.png" />
-        <div className=" flex w-full absolute z-30 -mt-96">
-          <div className="mt-60 self-end font-semibold text-5xl text-red-700 mb-2">
-            What we offer
-          </div>
-          <div className="ml-96 font-semibold text-5xl text-red-700 mb-2">
-            Buy Now <br />Pay Later Scheme
-          </div>
 
+        <div className=" flex w-full absolute z-30 mt-12">
+          <div className="w-3/5 mt-60">
+            <div className="pl-40 font-semibold text-5xl text-red-700 mb-8">
+              What we offer
+            </div>
+            <div className="pl-40 font-light text-xl text-gray-700 mb-2">
+              ChequeMate provide financial inclusion to SMEs by creating a platform facilitating Business to Business (B2B) BNPL scheme. We offers a safer alternative to taking high interest loans from banks, allowing you to procure goods from suppliers at no or low interest repayment instalments.
+            </div>
+          </div>
+          <div className="w-3/5">
+            <div className="pl-10 pr-40 font-semibold text-5xl text-red-700 mb-8">
+              Buy Now <br />Pay Later Scheme
+            </div>
+            <div className="pl-10 pr-40 font-light text-xl text-gray-700 mb-2">
+              BNPL services enable consumers to buy items now and spread their purchases over short instalment periods, with little to no interest.
+            </div>
+          </div>
         </div>
+        <img src="/landing/background.png" classNAme="absolute h-screen z-10" />
 
 
 
