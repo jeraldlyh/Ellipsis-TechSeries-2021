97f8534ed2baa3355dbc8bfd51ac6d5fce08414c
Oversparkling <nicholasongck@gmail.com>
2021-08-28 21:41:49 +0800

    UI Fix

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -22,7 +22,7 @@ export default function Login() {
                     className="-mt-20 rounded-lg shadow-md text-center bg-white justify-between items-center py-14 px-20 flex flex-row"
                 >
                     <div className="items-start flex flex-col mr-16">
-                        <div className="text-red-700 font-semibold text-2xl tracking-wider mb-1">
+                        <div className="text-red-700 font-semibold text-2xl tracking-wider mb-1 ">
                             LOGIN
                         </div>
                         <div
@@ -32,7 +32,7 @@ export default function Login() {
                             Sign in to continue your BNPL journey
                         </div>
                         <input
-                            className="mb-2 px-5 border"
+                            className="mb-2 px-5 border focus:outline-none"
                             style={{ width: 250, height: 40, borderRadius: 10, backgroundColor: '#F7F7F7', borderColor: '#B6B6B6' }}
                             placeholder="Email"
                             type="email"
@@ -40,7 +40,7 @@ export default function Login() {
                             onChange={e => setEmail(e.target.value)}
                         />
                         <input
-                            className="mb-8 px-5 border"
+                            className="mb-8 px-5 border focus:outline-none"
                             style={{ width: 250, height: 40, borderRadius: 10, backgroundColor: '#F7F7F7', borderColor: '#B6B6B6' }}
                             placeholder="Password"
                             type="password"
