08a6de01d6f624a3f512a61a5c6b34ffe3ade0ed
Felice <felicepng2010@gmail.com>
2021-08-28 21:58:20 +0800

    Edit listings page

--- frontend/components/ListingItem.js
+++ frontend/components/ListingItem.js
@@ -9,18 +9,19 @@ import { useState } from 'react'
 export default function ListingItem(props) {
   const router = useRouter();
   const [isCardView, setIsCardView] = useState(false)
+  const visible = "cursor-pointer"
+  const hidden = "cursor-pointer opacity-30"
 
   return (
-    <div className="cursor-pointer">
-      <img className="pb-5" src={props.image} width="240" height="330" onClick={() => { router.push({ pathname: "/listing/" + `${props.id}`, query: { name: props.name, company: props.company, price: props.price, image: props.image, desc: props.desc, bnpl: props.bnpl } }) }}/>
+    <div className={isCardView ? visible : hidden}>
+      <img className="pb-5" src={props.image} width="240" height="330" onClick={() => { router.push({ pathname: "/listing/" + `${props.id}`, query: { name: props.name, company: props.company, price: props.price, image: props.image, desc: props.desc, bnpl: props.bnpl } }) }} />
       <div className="flex justify-center">
 
-      <IconButton  
-        onClick={() => setIsCardView(!isCardView)}>
-        {isCardView ? <AiFillEye/>: <AiFillEyeInvisible/> }
-      </IconButton>
-       
-        
+        <IconButton
+          onClick={() => setIsCardView(!isCardView)}>
+          {isCardView ? <AiFillEye /> : <AiFillEyeInvisible />}
+        </IconButton>
+
       </div>
       <div onClick={() => { router.push({ pathname: "/listing/" + `${props.id}`, query: { name: props.name, company: props.company, price: props.price, image: props.image, desc: props.desc, bnpl: props.bnpl } }) }}>
         <div className={styles.productName}>{props.name}</div>
