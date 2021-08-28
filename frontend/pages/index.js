79650c3d22e0af93b289ac2ce5bbe8ed72bfe507
Felice <felicepng2010@gmail.com>
2021-08-28 13:36:02 +0800

    Add background image

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,26 +4,41 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen py-2">
       <Navbar />
-      <div className="bg-gray-50 w-screen">
+      <div className="bg-gray-100 w-screen justify-center">
         <div className="justify-between flex tracking-widest font-semibold text-7xl w-screen h-screen px-28">
           <div className="w-48 py-52 z-10">
             <div className="text-black mb-3">
               CHEQUEMATE
             </div>
-            <div className="text-red-600 mb-2">
+            <div className="text-red-700 mb-2">
               BNPL
             </div>
-            <div className="border-b-8 border-red-600" />
+            <div className="border-b-8 border-red-700" />
             <div className="py-2 px-5 w-32 mt-10 tracking-wider bg-black rounded-md text-white text-xs cursor-pointer hover:bg-gray-700" onClick={() => router.push("/")}>Register now</div>
           </div>
           <img src="/landing/building.png" className="pt-10 pb-24 h-screen" />
         </div>
-        <div className="text-center italic px-96">
-          We help <span className="text-red-600">small businesses</span> access <span className="text-red-600">Buy Now Pay Later (BNPL)</span> schemes with the aim of <span className="text-red-600">financial inclusion</span> in mind
+        <div className="flex justify-center">
+          <div className="text-center italic w-1/2">
+            We help <span className="text-red-700">small businesses</span> access <span className="text-red-700">Buy Now Pay Later (BNPL)</span> schemes with the aim of <span className="text-red-700">financial inclusion</span> in mind
+          </div>
         </div>
-        <div className="mt-8 text-red-600 font-light text-center text-xxs">
+        <div className="mt-8 mb-20 text-red-700 font-light text-center text-xxs">
           - CHEQUEMATE
         </div>
+        <img src="/landing/background.png" />
+        <div className=" flex w-full absolute z-30 -mt-96">
+          <div className="mt-60 self-end font-semibold text-5xl text-red-700 mb-2">
+            What we offer
+          </div>
+          <div className="ml-96 font-semibold text-5xl text-red-700 mb-2">
+            Buy Now <br />Pay Later Scheme
+          </div>
+
+        </div>
+
+
+
       </div>
     </div>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    mode: "jit",
-    purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-    darkMode: false, // or "media" or "class"
-    theme: {
-        extend: {
-          fontSize: {
-            'xxs': '.3rem'
-          },
-          fontFamily: {
-            serif: ["Alatsi"]
-          }
-        },
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or "media" or "class"
+  theme: {
+    extend: {
+      fontSize: {
+        'xxs': '.3rem'
+      },
+      fontFamily: {
+        serif: ["Alatsi"]
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 }
