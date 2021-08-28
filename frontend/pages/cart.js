2b4b0444a0b49e8dc5ac7a7dfed4aa13b1314ca5
Oversparkling <nicholasongck@gmail.com>
2021-08-28 15:58:44 +0800

    MyCart page

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,6 +1,14 @@
 import Navbar from "../components/Navbar";
-import { Divider } from "@material-ui/core";
-import { AiOutlinePlus,AiOutlineMinus, AiOutlineClose} from "react-icons/ai";
+import {
+    Divider,
+    FormControl,
+    FormLabel,
+    RadioGroup,
+    FormControlLabel,
+    Radio
+} from "@material-ui/core";
+import { AiOutlinePlus, AiOutlineMinus, AiOutlineClose } from "react-icons/ai";
+import CartItem from "../components/CartItem";
 
 export default function Home() {
     return (
@@ -9,7 +17,9 @@ export default function Home() {
             <div className="text-center justify-center">
                 <span style={{ color: "#D02626", fontSize: 60 }}>My Cart</span>
             </div>
+
             <div className="px-16">
+                {/* Header */}
                 <div className="px-16 flex-row flex justify-between">
                     <div className="w-36 flex-row flex justify-center">
                         <span>PRODUCT</span>
@@ -24,31 +34,58 @@ export default function Home() {
                     </div>
                 </div>
                 <Divider />
-                <div className="pl-16 mt-4 flex-row flex justify-between">
-                    <div className = "flex-row flex h-36">
-                        <img src="/flour.jpg" className="w-36" />
-                        <div className="justify-center flex-col flex ml-10">
-                            <span>Premium Flour</span>
-                            <span>S$ 10 / 100kg</span>
-                            <span>PonHockSG</span>
+                <CartItem />
+                <CartItem />
+
+                {/* Payment Plans */}
+                <div style = {{backgroundColor:"#E7E7E7"}} className="w-full mt-10 px-20 py-10 justify-between flex-row flex rounded-3xl shadow-md">
+                    <div className="flex-col flex">
+                        <span className = "mb-5 text-xl">Choose Payment Plans</span>
+                        <FormControl component="fieldset">
+                            <RadioGroup
+                                aria-label="gender"
+                                name="gender1"
+                            >
+                                <FormControlLabel
+                                    value="female"
+                                    control={<Radio />}
+                                    label="Get what you love now"
+                                />
+                                <FormControlLabel
+                                    value="male"
+                                    control={<Radio />}
+                                    label="Buy Now Pay Later"
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                        <div className = "text-center border rounded-lg">
+                            <span className = "text-xs">My BNPL deal</span>
                         </div>
                     </div>
-                    <div className = "flex-row flex">
-                        <div className = "w-48 flex-row flex items-center justify-evenly">
-                            <AiOutlinePlus/>
-                            <span>15</span>
-                            <AiOutlineMinus/>
+
+                    <div className="w-80 h-30 flex-col flex">
+                        <div className="flex-row flex justify-between mb-5">
+                            <span>SUBTOTAL</span>
+                            <span>$5150</span>
                         </div>
-                        <div className = "w-32 flex-row flex items-center justify-evenly">
-                            <span>$ 150</span>
+                        <div className="flex-row flex justify-between mb-5">
+                            <span>SHIPPING</span>
+                            <span>Free</span>
                         </div>
-                        <div className = "w-16 flex items-center justify-center">
-                            <AiOutlineClose />
+                        <Divider />
+                        <div className="flex-row flex justify-between mb-5">
+                            <span>TOTAL</span>
+                            <span>$5150</span>
+                        </div>
+                        <div
+                            className="text-center border rounded-lg"
+                            style={{ backgroundColor: "#D02626" }}
+                        >
+                            <span style={{ color: "white" }}>CHECK OUT</span>
                         </div>
                     </div>
                 </div>
             </div>
-            <div></div>
         </div>
     );
 }
