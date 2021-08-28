cc5e315fa09d49297a249fc95b70ab7ebe615511
Felice <felicepng2010@gmail.com>
2021-08-28 16:27:17 +0800

    Add marketplace

--- frontend/pages/marketplace.js
+++ frontend/pages/marketplace.js
@@ -1,83 +1,108 @@
 import styles from '../styles/marketplace.module.css'
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar.js'
-// import { Menu, Transition } from '@headlessui/react'
-// import { ChevronDownIcon } from '@heroicons/react/solid'
+import ReactDOM from "react-dom";
+import { GiMagnifyingGlass } from "react-icons/gi"
+import SortDropdown from '../components/SortDropdown.js'
+
+
 
 const marketplace = () => {
+    const [category, setCategory] = useState({
+        id: 1,
+        name: "Sort By..."
+    })
+    const data = [
+        {
+            id: 1,
+            name: "Price"
+        },
+        {
+            id: 2,
+            name: "Poo"
+        },
+        {
+            id: 3,
+            name: "Peepee"
+        },
+        {
+            id: 4,
+            name: "Monkey"
+        }
+    ]
+
     return (
         <div className={styles.background}>
             <Navbar></Navbar>
             <div className="pt-10"></div>
             <div className={styles.header1container}>
-                <div className={styles.header1}>
-                    Marketplace
+                <div className="text-red-700 font-semibold text-6xl text-center w-full tracking-widest pb-1 ">
+                    MARKETPLACE
                 </div>
-                <div className={styles.subheader}>
+                <div className="text-grey-700 text-sm text-center w-full tracking-wide">
                     "why pay now when you can pay later"
                 </div>
             </div>
-
-            <div className="pt-5 pb-7 relative mx-auto text-gray-600 flex justify-center pb-20">
-                <input className="border-0 bg-white h-10 w-full mx-96 px-10 pr-16 shadow-lg rounded-full text-sm focus:outline-none" 
-                placeholder="Find what you are looking for..."></input>
-                <button></button>
+            <div className="mx-96 bg-white flex items-center rounded-full shadow-lg px-3 space-x-3 py-2 my-10">
+                <div className="pl-1">
+                    <GiMagnifyingGlass />
+                </div>
+                <input className="w-full focus:outline-none" placeholder="Find what you are looking for..." />
             </div>
 
-            <div className="grid grid-cols-2 pb-7 pt-8 pl-32 pr-32">
-                <div className="text-lg">Featured</div>
+            <div className="flex justify-between pb-7 pt-8 pl-32 pr-32">
+                <div className="text-xl font-semibold">Featured</div>
                 <div>
-                    <div className="text-sm text-right">Sort By: </div>
-                    
+                    <SortDropdown data={data} selected={category} setSelected={setCategory} />
                 </div>
-                
+
             </div>
 
             <div className="grid gap-y-16 grid-cols-4 justify-items-center p-0.5 pl-20 pr-20 pb-30">
                 <div>
-                    <img className="pb-5" src="/photos/marketplace/flour.jpeg" width="240" height="330"/>
+                    <img className="pb-5" src="/photos/marketplace/flour.jpeg" width="240" height="330" />
                     <div className={styles.productName}>Premium Flour</div>
                     <div className={styles.companyName}>PonHockSG</div>
                     <div className={styles.price}>S$ 10 / 100kg</div>
                 </div>
 
                 <div>
-                    <img className="pb-5" src="/photos/marketplace/cups.jpeg" width="240" height="330"/>
+                    <img className="pb-5" src="/photos/marketplace/cups.jpeg" width="240" height="330" />
                     <div className={styles.productName}>Ceramic Cups</div>
                     <div className={styles.companyName}>Cupping</div>
                     <div className={styles.price}>S$ 10 / pc</div>
                 </div>
 
                 <div>
-                    <img className="pb-5" src="/photos/marketplace/chair.jpeg" width="240" height="330"/>
+                    <img className="pb-5" src="/photos/marketplace/chair.jpeg" width="240" height="330" />
                     <div className={styles.productName}>Modern Office Chair</div>
                     <div className={styles.companyName}>ChairMan</div>
                     <div className={styles.price}>S$ 250 / pc</div>
                 </div>
 
                 <div>
-                    <img className="pb-5" src="/photos/marketplace/backpack.jpeg" width="240" height="330"/>
+                    <img className="pb-5" src="/photos/marketplace/backpack.jpeg" width="240" height="330" />
                     <div className={styles.productName}>Everyday Bag</div>
                     <div className={styles.companyName}>BigBagCompany</div>
                     <div className={styles.price}>S$ 45 / pc</div>
                 </div>
 
                 <div>
-                    <img className="pb-5" src="/photos/marketplace/handbag.jpeg" width="240" height="330"/>
+                    <img className="pb-5" src="/photos/marketplace/handbag.jpeg" width="240" height="330" />
                     <div className={styles.productName}>Estla hand bag</div>
                     <div className={styles.companyName}>BigBagCompany</div>
                     <div className={styles.price}>S$ 30 / pc</div>
                 </div>
 
                 <div>
-                    <img className="pb-5" src="/photos/marketplace/totebag.jpeg" width="240" height="330"/>
+                    <img className="pb-5" src="/photos/marketplace/totebag.jpeg" width="240" height="330" />
                     <div className={styles.productName}>Cowhide hand bag</div>
                     <div className={styles.companyName}>ECCOwide</div>
                     <div className={styles.price}>S$ 45 / pc</div>
                 </div>
 
                 <div>
-                    <img className="pb-5" src="/photos/marketplace/bottle.jpeg" width="240" height="330"/>
+                    <img className="pb-5" src="/photos/marketplace/bottle.jpeg" width="240" height="330" />
                     <div className={styles.productName}>Memobottle</div>
                     <div className={styles.companyName}>Bottles and bottles</div>
                     <div className={styles.price}>S$ 20 / pc</div>
@@ -89,7 +114,7 @@ const marketplace = () => {
                     <div className={styles.companyName}>Plotting pots</div>
                     <div className={styles.price}>S$ 15 / pc</div>
                 </div>
-            
+
             </div>
             <div className="pt-32"></div>
         </div>
--- frontend/pages/product.js
+++ frontend/pages/product.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Navbar from '../components/Navbar.js'
-import styles from '../styles/product.module.css'
+import styles from '../styles/marketplace.module.css'
 
 const product = () => {
     return (
         <div className={styles.background}>
             <Navbar></Navbar>
-            <div >
-                
+            <div className = "grid grid-cols-2 flex justify-center">
+                <img class="ml-44 flex justify-end" src="/photos/marketplace/flour.jpeg" width="330" height="445"/>
+
+                <div className="pb-5 p-20"> 
+                    <div className={styles.companyName2}>PonHockSG</div>
+                    <div className={styles.productName2}>Premium Flour</div>
+                    <div className={styles.price2}>S$ 10 / 100kg</div>
+
+                    <div className={styles.detail}>At PonHockSG, we provide the best flour you can find in Singapore, in terms of product quality, rice texture and product sustainability.</div>
+                    <div className={styles.price2}>Quantity: </div>
+                 </div>
             </div>
         </div>
     )
