ecc4f72f489177d86c7ee53d59b6e78c374c278c
Felice <felicepng2010@gmail.com>
2021-08-28 11:37:16 +0800

    Update navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -1,11 +1,17 @@
+import { useRouter } from "next/router"
+
 export default function Navbar() {
+  const router = useRouter()
+
   return (
-    <div className="font-semibold flex bg-white items-center justify-between px-10 pt-1 pb-3">
-      <div className="tracking-widest">CHEQUEMATE</div>
+    <div className="top-0 sticky font-semibold flex bg-white items-center justify-between px-10 pt-1 pb-3">
+      <div className="tracking-widest cursor-pointer" onClick={() => router.push("/")}>
+        CHEQUEMATE
+      </div>
       <div className="flex items-center justify-between w-56">
-        <div className="tracking-wider text-xs">Help</div>
-        <div className="tracking-wider text-xs">Login</div>
-        <div className="py-2 px-5 tracking-wider bg-black rounded-md text-white text-xs">Register</div>
+        <div className="tracking-wider text-xs cursor-pointer" onClick={() => router.push("/")}>Help</div>
+        <div className="tracking-wider text-xs cursor-pointer" onClick={() => router.push("/")}>Login</div>
+        <div className="py-2 px-5 tracking-wider bg-black rounded-md text-white text-xs cursor-pointer hover:bg-gray-700" onClick={() => router.push("/")}>Register</div>
       </div>
     </div>
   )
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -4,7 +4,17 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen py-2">
       <Navbar />
-      <div className="bg-gray-50 w-screen h-screen">test</div>
+      <div className="bg-gray-50 tracking-widest font-semibold text-7xl w-screen h-screen px-20 py-52">
+        <div className="w-48">
+          <div className="text-black mb-2">
+            CHEQUEMATE
+          </div>
+          <div className="text-red-600 mb-1">
+            BNPL
+          </div>
+          <div className="border-b-8 border-red-600" />
+        </div>
+      </div>
     </div>
   )
 }
