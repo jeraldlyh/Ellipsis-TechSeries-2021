2386a7f535a19eae75c7a0d8e2c93785255bc052
Oversparkling <nicholasongck@gmail.com>
2021-08-28 18:49:17 +0800

    Update auth.js

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -80,5 +80,11 @@ module.exports = {
         refreshTokens = _.filter(refreshTokens, function(t) {
             return t !== token
         })
+    },
+    creditRatingCalculation: function(req,res){
+        const {quickRatio,netProfitMargin,debtRatio,operatingCashFlowPerShare} = req.body
+        Number("4.874915326E7")
+        const creditRating = Number("-1.0595E-2") * quickRatio + Number("6.475440E-2") * netProfitMargin +  Number("-1.47744E1") * debtRatio + Number("-3.6729E-7") * operatingCashFlowPerShare
+        return creditRating;
     }
 }
