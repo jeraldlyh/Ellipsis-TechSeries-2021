9c8305c32b2c905604fb36fcd635dc9fc88aca22
Felice <felicepng2010@gmail.com>
2021-08-28 14:05:06 +0800

    marketplace iter1

---
+++ frontend/pages/product.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar.js'
+import styles from '../styles/product.module.css'
+
+const product = () => {
+    return (
+        <div className={styles.background}>
+            <Navbar></Navbar>
+            <div >
+                
+            </div>
+        </div>
+    )
+}
+
+export default product
