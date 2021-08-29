8612b53d32857df2908cbd9f26a0febb858d1eb5
Oversparkling <nicholasongck@gmail.com>
2021-08-29 15:19:24 +0800

    company backend api

--- backend/controller/company.js
+++ backend/controller/company.js
@@ -4,13 +4,20 @@ const _ = require("lodash")
 
 module.exports = {
     creditRatingCalculation: async function (req, res) {
-        const { quickRatio, netProfitMargin, debtRatio, operatingCashFlowPerShare } = req.body
-        Number("4.874915326E7")
+        const { quickRatio, netProfitMargin, debtRatio, operatingCashFlowPerShare,companyUEN } = req.body
         const creditRating = Number("-1.0595E-2") * quickRatio + Number("6.475440E-2") * netProfitMargin + Number("-1.47744E1") * debtRatio + Number("-3.6729E-7") * operatingCashFlowPerShare
-        await Models.Company.update({
-            creditRating: creditRating.toFixed(2)
+        var existingCompany = await Models.Company.findOne({
+            where: {
+                UEN: companyUEN
+            }
         })
+        if (existingCompany){
+            await existingCompany.update({
+                creditRating : creditRating.toFixed(2)
+            })
+        }
         return res.sendStatus(200)
+
     },
     getProductByID: async function (req, res) {
         const products = await Models.Product.findAll({
